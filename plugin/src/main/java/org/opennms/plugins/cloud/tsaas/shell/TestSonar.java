10ce239038c4ea440e239621d2458c06293ebc2c
Maxim Brener <mbrener@opennms.com>
2022-07-04 13:16:58 -0400

    sonar tests

--- plugin/src/main/java/org/opennms/plugins/cloud/tsaas/shell/TestSonar.java
+++ plugin/src/main/java/org/opennms/plugins/cloud/tsaas/shell/TestSonar.java
@@ -1,9 +1,12 @@
 package org.opennms.plugins.cloud.tsaas.shell;
 
+import java.sql.*;
+
 public class TestSonar {
 
     public static boolean func(String[] args) {
-        if (args.length> 0 && "true".equals(args[0])){
+        String s = "true";
+        if (args.length> 0 && s.equals(args[0])){
             return true;
         } else {
             return false;
@@ -11,10 +14,45 @@ public class TestSonar {
     }
 
     public static boolean func2(String[] args) {
-        if (args.length> 0 && "true".equals(args[0])){
+        String s = "false";
+        if (args.length> 0 && s.equals(args[0])){
             return true;
         } else {
             return false;
         }
+    }
+
+    public static boolean func3(String s) {
+        if (s.equals("abc")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public User getUser(Connection con, String user) throws SQLException {
+
+        Statement stmt1 = null;
+        Statement stmt2 = null;
+        PreparedStatement pstmt;
+        try {
+            stmt1 = con.createStatement();
+            ResultSet rs1 = stmt1.executeQuery("GETDATE()"); // No issue; hardcoded query
+
+            stmt2 = con.createStatement();
+            ResultSet rs2 = stmt2.executeQuery("select FNAME, LNAME, SSN " +
+                    "from USERS where UNAME=" + user);  // Sensitive
+
+            pstmt = con.prepareStatement("select FNAME, LNAME, SSN " +
+                    "from USERS where UNAME=" + user);  // Sensitive
+            ResultSet rs3 = pstmt.executeQuery();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    private class User {
     }
 }
