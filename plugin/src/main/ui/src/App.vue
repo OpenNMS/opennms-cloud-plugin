e4d1c6fe2353488eead18f7467265379add6d944
patrick.schweizer <patrick@schweizer-ing.com>
2022-07-29 18:04:43 +0200

    DC-216: Add to Cloud Plugin configuration web page: init project

---
+++ plugin/src/main/ui/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <router-view></router-view>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
