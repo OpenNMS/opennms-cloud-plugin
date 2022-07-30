c6f85e32d2e9e04840e6c5a4ae79f74a1d23bf7f
patrick.schweizer <patrick@schweizer-ing.com>
2022-07-30 18:53:37 +0200

    DC-216: Add to Cloud Plugin configuration web page: add rest end points

---
+++ plugin/src/main/java/org/opennms/plugins/cloud/rest/CloudConfigRestService.java
@@ -0,0 +1,19 @@
+package org.opennms.plugins.cloud.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/cloud/config")
+public interface CloudConfigRestService {
+    @GET()
+    @Produces(value={MediaType.APPLICATION_JSON})
+    Response getConfig();
+
+    @PUT
+    @Path("/activationkey")
+    Response putActivationKey(final String key);
+}
