6dec2bc51383c6b0052e07b3f7d0f7ed13af40cc
patrick.schweizer <patrick@schweizer-ing.com>
2022-08-06 16:48:25 +0200

    DC-243: Enable / disable cloud services independent of the Cloud Plugin itself: add more test coverage

--- plugin/src/main/java/org/opennms/plugins/cloud/ServiceManager.java
+++ plugin/src/main/java/org/opennms/plugins/cloud/ServiceManager.java
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
 package org.opennms.plugins.cloud;
 
 import java.util.Hashtable;
@@ -41,7 +69,7 @@ public class ServiceManager {
         LOG.info("Registered tsaas with OpenNMS.");
     }
 
-    public void stopTsaas() {
+    public void deregisterTsaas() {
         if (this.tsaasRegistration == null) {
             LOG.warn("Cannot deregister tsaas, it's not registered.");
             return;
@@ -53,6 +81,6 @@ public class ServiceManager {
 
     // called via blueprint
     public void destroy() {
-        stopTsaas();
+        deregisterTsaas();
     }
 }
---
+++ plugin/src/test/java/org/opennms/plugins/cloud/ServiceManagerTest.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.plugins.cloud;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opennms.integration.api.v1.timeseries.TimeSeriesStorage;
+import org.opennms.plugins.cloud.tsaas.TsaasStorage;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class ServiceManagerTest {
+
+    private BundleContext context;
+    private TsaasStorage tsaas;
+    private ServiceManager manager;
+    ServiceRegistration<TimeSeriesStorage> registration;
+
+    @Before
+    public void setUp() {
+        context = mock(BundleContext.class);
+        registration = mock(ServiceRegistration.class);
+        tsaas = mock(TsaasStorage.class);
+        when(context.registerService(eq(TimeSeriesStorage.class), eq(tsaas), any())).thenReturn(registration);
+        manager = new ServiceManager(context, tsaas); // registers tsaas
+
+    }
+
+    @Test
+    public void shouldRegisterOnlyWhenNotRegistered() {
+        verify(context, times(1)).registerService(eq(TimeSeriesStorage.class), eq(tsaas), any());
+        reset(context);
+        manager.registerTsaas();
+        verify(context, never()).registerService(eq(TimeSeriesStorage.class), eq(tsaas), any());
+    }
+
+    @Test
+    public void shouldDeregisterOnlyWhenRegistered() {
+        manager.deregisterTsaas();
+        verify(registration, times(1)).unregister();
+        reset(registration);
+        manager.deregisterTsaas();
+        verify(registration, never()).unregister(); // we already deregisterd => no more deregistering
+    }
+
+    @Test
+    public void destroyShouldDeregisterAllServices() {
+        manager.destroy();
+        verify(registration, times(1)).unregister();
+    }
+
+}
