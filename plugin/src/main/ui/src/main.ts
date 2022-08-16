abf7804b29b9db7656acc6cffe565e2bf2e89a99
Patrick Schweizer <patrick@schweizer-ing.com>
2022-08-16 00:51:00 -0600

    DC-227: Implement UI
    Co-authored-by: Scott Thompson <scthompson@opennms.com>

--- plugin/src/main/ui/src/main.ts
+++ plugin/src/main/ui/src/main.ts
@@ -2,7 +2,8 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import router from './router'
 import { createPinia } from 'pinia'
-
+import "@featherds/styles";
+import "@featherds/styles/themes/open-light.css";
 const envMode = import.meta.env.MODE
 
 //@ts-ignore
