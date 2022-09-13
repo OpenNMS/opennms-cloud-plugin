38319a92eb2f1ed3c3231199cf993771d257e6eb
Ryan Reabel <rreabel@opennms.com>
2022-09-13 17:10:19 -0400

    Adding pagination, Generalized Data to table, and table to the main CloudPlugin Page

--- plugin/src/main/ui/src/components/ApplianceRefresh.vue
+++ plugin/src/main/ui/src/components/ApplianceRefresh.vue
@@ -1,22 +1,17 @@
 <template>
+    <br/>
     <FeatherButton>
         Check for New Appliances
     </FeatherButton>
     <div>
       <table
-        class="tc1 tr2 tc4 tr6"
-        :class="{ condensed: condensed, striped: striped, hover: hover }"
+        :class="{ 'tc1 tr2 tc4 tr6': true, hover: true }"
       >
         <thead>
           <tr>
-            <FeatherSortHeader
-              scope="col"
-              property="ApplianceName"
-              :sort="sortAppliances"
-              v-on:sort-changed="sortChanged"
-            >
+            <th scope="col">
               Appliance Name
-            </FeatherSortHeader>
+          </th>
             <th scope="col">Node Id</th>
             <th scope="col">IP Address</th>
             <th scope="col">Location</th>
@@ -24,12 +19,12 @@
           </tr>
         </thead>
         <tbody>
-          <tr :class="{ selected: selected }">
-            <td>virtual-appliance-elara</td>
-            <td>IP Address</td>
-            <td>192.168.4.11</td>
-            <td>Kanata_Office</td>
-            <td>UP / UP</td>
+          <tr v-for="appliance in appliances">
+            <td>{{ appliance.applianceLabel }}</td>
+            <td>{{ appliance.nodeId }}</td> 
+            <td>{{ appliance.nodeIpAddress }}</td>
+            <td>{{ appliance.nodeLocation }}</td>
+            <td>{{ appliance.nodeStatus }}</td>
           </tr>
         </tbody>
       </table>
@@ -40,22 +35,14 @@
 <script setup lang="ts">
 import { FeatherButton } from '@featherds/button';
 import { FeatherPagination } from "@featherds/pagination";
-import Face from "@featherds/icon/action/AccountCircle";
-import { FeatherSortHeader, SORT } from "@featherds/table";
-
-const props = defineProps({
-      striped: false,
-      condensed: false,
-      hover: false,
-      selected: false,
-      icon: Face,
-      all: false,
-      checkbox1: true,
-      checkbox2: false,
-      sortTime: SORT.ASCENDING,
-      sortDate: SORT.NONE,
-      sortIcon: SORT.NONE,    
-})
+import { ref } from 'vue';
+const appliances = ref([{
+  applianceLabel: 'virtual-appliance-1',
+  nodeId: 4,
+  nodeIpAddress: '192.168.3.1',
+  nodeLocation: 'KanataOffice',
+  nodeStatus: 'UP',
+}])
 </script>
 
 <style lang="scss" scoped>
