545f7e7fe19abbcecb3b6adabf2950e908341bf8
Freddy Chu <fchu@opennms.com>
2022-09-29 17:47:47 -0400

    DC-136: clean up debug message

--- it-test/src/test/java/org/opennms/plugins/cloud/tsaas/TsaasStorageIT.java
+++ it-test/src/test/java/org/opennms/plugins/cloud/tsaas/TsaasStorageIT.java
@@ -34,48 +34,15 @@ public class TsaasStorageIT {
   private KarafShell karafShell;
 
   @BeforeClass
-  public static void beforeAll() throws IOException, InterruptedException {
-    String userHome = System.getProperty("user.home");
-    Process p = Runtime.getRuntime().exec("/usr/bin/uname -a");
-
-    BufferedReader stdInput = new BufferedReader(new
-            InputStreamReader(p.getInputStream()));
-
-    BufferedReader stdError = new BufferedReader(new
-            InputStreamReader(p.getErrorStream()));
-
-// Read the output from the command
-    System.out.println("Here is the standard output of the command:\n");
-    String s = null;
-    while ((s = stdInput.readLine()) != null) {
-      System.out.println(s);
-    }
-
-// Read any errors from the attempted command
-    System.out.println("Here is the standard error of the command (if any):\n");
-    while ((s = stdError.readLine()) != null) {
-      System.out.println(s);
-    }
-    LOG.info("Starting docker-compose with \"USER_HOME\"={}", System.getProperty("user.home"));
-    File karFile = new File(userHome, "/.m2/repository/org/opennms/plugins/cloud/kar/1.0.0-SNAPSHOT/kar-1.0.0-SNAPSHOT.kar");
-    LOG.info("Checking for Kar file: {}: File {}", karFile, karFile.exists());
+  public static void beforeAll() {
     environment = new DockerComposeContainer<>(new File("src/test/resources/docker-compose.yaml"))
-        .withEnv("USER_HOME", userHome)
+        .withEnv("USER_HOME", System.getProperty("user.home"))
         .withTailChildContainers(true)
         .withExposedService("database_1", 5432, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(15)))
         .withLogConsumer("database_1", new Slf4jLogConsumer(LOG))
         .withExposedService("horizon_1", 8980, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(240)))
         .withLogConsumer("horizon_1", new Slf4jLogConsumer(LOG));
-    try {
-      environment.start();
-    } catch(Exception ex){
-      ex.printStackTrace();
-      var container = environment.getContainerByServiceName("horizon_1");
-      if(container.isPresent()){
-        LOG.info("isCreated:{}, isRunning: {}, isHealthy: {}", container.get().isCreated(), container.get().isRunning(), container.get().isHealthy());
-        LOG.info(container.get().getLogs());
-      }
-    }
+    environment.start();
   }
 
   @Before
