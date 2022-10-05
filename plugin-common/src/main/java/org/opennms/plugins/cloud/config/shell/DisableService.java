0d3b09bd9880f7636ada7733f446686d80fc49b4
patrick.schweizer <patrick@schweizer-ing.com>
2022-10-05 07:27:09 -0600

    DC-314: Make naming of modules, folders and features consistent

---
+++ plugin-common/src/main/java/org/opennms/plugins/cloud/config/shell/DisableService.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.plugins.cloud.config.shell;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.opennms.plugins.cloud.srv.RegistrationManager;
+
+@Command(scope = "opennms-cloud", name = "disableService", description = "Disables a Cloud Service (e.g. Tsaas)")
+@Service
+@SuppressWarnings("java:S106") // System.out is used intentionally: we want to see it in the Karaf shell
+public class DisableService implements Action {
+
+    @Reference
+    private RegistrationManager manager;
+
+    @Argument()
+    String service;
+
+    @Override
+    public Object execute() throws Exception {
+        if (RegistrationManager.Service.contains(this.service)) {
+            manager.deregister(RegistrationManager.Service.valueOf(service));
+        } else {
+            System.out.println("Unknown service " + service);
+        }
+        return null;
+    }
+
+}
