5f604d8f0fe9383d043c68e1de762e672f74133f
Freddy Chu <fchu@opennms.com>
2022-10-12 01:41:55 -0400

    DC-301: add rpm post install script and fix debian

--- assembly/common-resources/scripts/postinst.opennms.deb.sh
+++ assembly/common-resources/scripts/postinst.opennms.deb.sh
@@ -4,9 +4,11 @@ OPENNMS_HOME="/usr/share/opennms"
 
 if [ -f "$OPENNMS_HOME/etc/opennms.conf" ]; then
   . "$OPENNMS_HOME/etc/opennms.conf"
+else
+  RUNAS="opennms"
 fi
 
 chown -R $RUNAS "$OPENNMS_HOME/etc/examples/opennms.properties.d"
 
 echo "Please make sure org.opennms.timeseries.strategy=integration"
-echo "Example config is located at. $OPENNMS_HOME/etc/examples/opennms.properties.d/timeseries.properties"
+echo "Example config is located at $OPENNMS_HOME/etc/examples/opennms.properties.d/timeseries.properties"
---
+++ assembly/common-resources/scripts/postinst.opennms.rpm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OPENNMS_HOME="/opt/opennms"
+
+if [ -f "$OPENNMS_HOME/etc/opennms.conf" ]; then
+  . "$OPENNMS_HOME/etc/opennms.conf"
+else
+  RUNAS="opennms"
+fi
+
+chown -R $RUNAS "$OPENNMS_HOME/etc/examples/opennms.properties.d"
+
+echo "Please make sure org.opennms.timeseries.strategy=integration"
+echo "Example config is located at $OPENNMS_HOME/etc/examples/opennms.properties.d/timeseries.properties"
