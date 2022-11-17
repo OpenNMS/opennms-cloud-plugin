6d364f7a20aadb83c9abd2819724bfc21be00a64
synqotik <stheleman@opennms.com>
2022-11-16 22:02:44 -0500

    NMS-14996: Enable routing for multiple plugins

--- plugin-core/src/main/ui/src/main.ts
+++ plugin-core/src/main/ui/src/main.ts
@@ -7,7 +7,7 @@ import "@featherds/styles/themes/open-light.css";
 const envMode = import.meta.env.MODE
 
 //@ts-ignore
-window['uiextension'] = App
+window['cloudUiExtension'] = App
 
 // used to run plugin by itself
 if (envMode === 'development') {
