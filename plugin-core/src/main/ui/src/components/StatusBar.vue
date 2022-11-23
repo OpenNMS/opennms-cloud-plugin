b704cfb9243f5768975b7875e4e8d70d0b17f82a
Ryan Reabel <82829460+nmsrreabel@users.noreply.github.com>
2022-11-23 18:25:46 -0500

    CLOUD-2754: rough draft of status bar

---
+++ plugin-core/src/main/ui/src/components/StatusBar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const status = ref('deactivated');
+
+</script>
+
+<template>
+    <div v-if="status === 'deactivated'" class="deactivated statusbar">
+        <FeatherIcon>Not activated</FeatherIcon>
+    </div>
+    <div v-if="status === 'activating'" class="activating">
+        <FeatherIcon>Activating...</FeatherIcon>
+    </div>
+    <div v-if="status === 'activated'" class="activated">
+        <FeatherIcon />
+        Activated
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+.deactivated {
+    background-color: grey;
+}
+
+.activating {
+    background-color: lightblue;
+}
+
+.activated {
+    background-color: green;
+}
+
+.statsbar {
+    border-radius: 1rem;
+}
+
+</style>
