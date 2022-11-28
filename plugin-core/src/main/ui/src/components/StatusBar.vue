a35d722c841780f2270c2498855ea926c548fe1c
Ryan Reabel <82829460+nmsrreabel@users.noreply.github.com>
2022-11-28 16:10:52 -0500

    CLOUD-2754: adding designs based on figma, placeholder icons

--- plugin-core/src/main/ui/src/components/StatusBar.vue
+++ plugin-core/src/main/ui/src/components/StatusBar.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-
-const status = ref('deactivated');
+import CheckCircle from '@featherds/icon/action/CheckCircle';
+import Cancel from '@featherds/icon/action/Cancel';
+import Cycle from '@featherds/icon/action/Cycle';
+const status = ref('activated');
 
 </script>
 
 <template>
     <div v-if="status === 'deactivated'" class="deactivated statusbar">
-        <FeatherIcon>Not activated</FeatherIcon>
+        <FeatherIcon :icon="Cancel" />Not activated
     </div>
-    <div v-if="status === 'activating'" class="activating">
-        <FeatherIcon>Activating...</FeatherIcon>
+    <div v-if="status === 'activating'" class="activating statusbar">
+        <FeatherIcon :icon="Cycle"/>Activating...
     </div>
-    <div v-if="status === 'activated'" class="activated">
-        <FeatherIcon />
+    <div v-if="status === 'activated'" class="activated statusbar">
+        <FeatherIcon :icon="CheckCircle"/>
         Activated
     </div>
 </template>
@@ -21,19 +23,40 @@ const status = ref('deactivated');
 <style scoped lang="scss">
 
 .deactivated {
-    background-color: grey;
+    background-color: rgba(117, 117, 117, 0.2);
+    color: #757575;
+    width: 115px;
 }
 
 .activating {
-    background-color: lightblue;
+    background-color: rgba(0, 146, 199, 0.2);
+    color: #0092C7;
+    width: 105px;
 }
 
 .activated {
-    background-color: green;
+    background: rgba(11, 114, 12, 0.2);
+    color: #0B720C;
+    width: 91 px;
 }
 
-.statsbar {
-    border-radius: 1rem;
+.statusbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 4px 6px;
+    // border-radius: 0.5rem;
+    height: 28px;
+    border-radius: 4px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 15px;
+
+    /* identical to box height */
+    font-feature-settings: 'tnum' on, 'lnum' on, 'cv05' on;
 }
 
 </style>
