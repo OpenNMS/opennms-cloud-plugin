b2997fd5caba560b5feb0fb43e3a504f876b3fa1
Ryan Reabel <82829460+nmsrreabel@users.noreply.github.com>
2023-02-09 15:27:45 -0500

    adding comment in order to trigger a build

--- plugin-core/src/main/ui/vite.config.ts
+++ plugin-core/src/main/ui/vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {viteExternalsPlugin} from 'vite-plugin-externals'
-
+//vite-plugin-externals is failing in build
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
